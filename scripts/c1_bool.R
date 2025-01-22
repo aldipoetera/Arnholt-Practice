bdc4d82001c834b4fab0175f1fd4ec64e4dc5ce8
aldipoetera <aldipoeteradius@gmail.com>
2025-01-23 00:27:32 +0700

    add few random scripts

---
+++ scripts/c1_bool.R
@@ -0,0 +1,14 @@
+X <- c(FALSE, TRUE, FALSE, TRUE)
+Y <- c(FALSE, FALSE, TRUE, TRUE)
+
+X & Y
+
+X | Y
+
+X == Y
+
+X != Y
+
+X && Y
+
+X[2]
---
+++ scripts/c1_first.R
@@ -0,0 +1,45 @@
+getwd()
+
+library(PASWR2)
+
+some_var <- 5 # this is a comment
+some_var # this is also a comment
+
+2 + c()
+
+c(1, 2, 3, 4)
+
+c(1, 2, 3, 4) + 3
+
+x <- 5
+y <- c(7, 3, 5)
+x + y
+
+z <- c(2, 3)
+w <- c(1, 1, 2, 2, 3)
+z + w
+
+typeof(c(TRUE, FALSE, TRUE))
+
+round(3.1242542, 3)
+
+options(width = 70)
+1:50
+c(3, 4, 5, 6, 7) <= c(7, 6, 5, 4, 3)
+
+c(c(1, 2), c(3, 4))
+c(c(1, 2), c(TRUE, FALSE))
+
+as.integer(3.42343)
+
+as.logical(as.integer(0.423))
+
+as.character(pi)
+as.character(4.5 + 33i)
+as.character(FALSE)
+
+vector_index <- c(1,3,4,6)
+vector_index[0]
+vector_index[1]
+
+
